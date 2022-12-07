20b93248446762a3131f07f5cc878be1cbb0f012
DimAn <diman@diman.io>
2022-12-07 22:09:53 +0300

    only mina

--- pginit/init-mina-db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-createdb mina
-#psql -d mina -f <(curl -Ls https://raw.githubusercontent.com/MinaProtocol/mina/master/src/app/archive/create_schema.sql)
-psql -d mina -f <(cat /docker-entrypoint-initdb.d/mina_schema_sql)
